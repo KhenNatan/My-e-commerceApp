8347254137f8e754d9e56e198c35e0296d60fe7a
KhenNatan <gwenyapearl@gmail.com>
2025-05-13 16:07:20 +0200

    this is my first java file for the app

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, Please scroll down to find the full options.");
+    }
+}
