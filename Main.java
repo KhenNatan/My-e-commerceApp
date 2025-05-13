9980651509194e857c62b9adacaae92f3e55733f
KhenNatan <gwenyapearl@gmail.com>
2025-05-13 16:16:57 +0200

    added a multiply function of two integers

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 import java.util.*;
 
 public class Main{
-
+    
+    public void multiply(int x, int y){
+        
+        System.out.println(x * y);
+    }
     public static void main(String[] args){
 
         System.out.println("Hello, Please scroll down to find the full options.");
+        
+       new Main().multiply(2,6);
     }
+
 }
