e9b1be67a592498afe0d00d856389ce8e9aa8a5d
KhenNatan <gwenyapearl@gmail.com>
2025-05-13 16:39:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,17 @@ public class Main{
         
         System.out.println(x * y);
     }
+    public static int addition(int x, int y, int k){
+        
+        return x + y + k;
+    }
     public static void main(String[] args){
 
         System.out.println("Hello, Please scroll down to find the full options.");
         
        new Main().multiply(2,6);
+
+       System.out.println(addition(3,4, 3));
     }
 
 }
